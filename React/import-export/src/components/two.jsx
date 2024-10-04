ec5bdd19e758b3292fb3911d8088ca6631e273e1
Riddhesh <riddhesh.chaudhary@gmail.com>
2024-10-04 19:12:59 +0530

    import-export of components

---
+++ React/import-export/src/components/two.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function two() {
+    return (
+        <div>
+            <h1>my Car</h1>
+            <img src="https://m.economictimes.com/thumb/msid-110533985,width-1200,height-900,resizemode-4,imgsize-58280/porsche.jpg" alt="" />
+        </div>
+    );
+}
